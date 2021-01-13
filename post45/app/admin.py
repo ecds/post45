332489833def554b6be43053c12136c0fef3136a
Sara Palmer <sara.palmer@emory.edu>
2021-01-13 10:43:25 -0500

    add Program Era models

--- post45/app/admin.py
+++ post45/app/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 from admin_numeric_filter.admin import NumericFilterModelAdmin, SingleNumericFilter, RangeNumericFilter, SliderNumericFilter
-from .models import Record
+from .models import Record, ProgramEraRecord, ProgramEraPeople, ProgramEraGr
 
 
 class RecordResource(resources.ModelResource):
@@ -19,3 +19,42 @@ class RecordAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     #     ('latestcomp', SliderNumericFilter),
     # )
 admin.site.register(Record, RecordAdmin)
+
+
+class ProgramEraRecordResource(resources.ModelResource):
+
+    class Meta:
+        model = ProgramEraRecord
+
+class ProgramEraRecordAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+        resource_class = ProgramEraRecordResource
+        list_display = ('docid', 'title', 'authors', 'date')
+        search_fields = ('docid', 'title', 'authors', 'date')
+
+admin.site.register(ProgramEraRecord, ProgramEraRecordAdmin)
+
+
+class ProgramEraPeopleResource(resources.ModelResource):
+
+    class Meta:
+        model = ProgramEraPeople
+
+class ProgramEraPeopleAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    resource_class = ProgramEraPeopleResource
+    list_display = ('person_id', 'full_name', 'gender', 'country')
+    search_fields = ('person_id', 'full_name', 'gender', 'country')
+
+admin.site.register(ProgramEraPeople, ProgramEraPeopleAdmin)
+
+
+class ProgramEraGraduationsResource(resources.ModelResource):
+
+    class Meta:
+        model = ProgramEraGraduations
+
+class ProgramEraGraduationsAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    resource_class = ProgramEraGraduationsResource
+    list_display = ('record_id', 'graduate_name', 'program', 'degree', 'thesis_title', 'genre')
+    search_fields = ('record_id', 'graduate_name', 'program', 'degree', 'thesis_title', 'genre')
+
+admin.site.register(ProgramEraGraduations, ProgramEraGraduationsAdmin)
--- post45/app/models.py
+++ post45/app/models.py
@@ -32,3 +32,52 @@ class Record(models.Model):
 
     def __str__(self):
         return self.docid
+
+class ProgramEraRecord(models.Model):
+    docid = models.CharField(max_length=255, blank=True, null=True)
+    authors = models.CharField(max_length=255, blank=True, null=True)
+    title = models.TextField(blank=True, null=True)
+    imprint = models.TextField(blank=True, null=True)
+    date = models.PositiveIntegerField(blank=True, null=True)
+    place = models.CharField(max_length=255, blank=True, null=True)
+    subjects = models.TextField(blank=True, null=True)
+    geographics = models.CharField(max_length=255, blank=True, null=True)
+    genres = models.TextField(max_length=255, blank=True, null=True)
+    classifications = models.TextField(max_length=255, blank=True, null=True)
+    locnum = models.CharField(max_length=255, blank=True, null=True)
+    oclc = models.CharField(max_length=255, blank=True, null=True)
+    isbn = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.docid
+
+
+class ProgramEraPeople(models.Model):
+    person_id = models.PositiveIntegerField(primary_key=True)
+    full_name = models.CharField(max_length=255, blank=True, null=True)
+    given_name = models.CharField(max_length=255, blank=True, null=True)
+    middle_name	= models.CharField(max_length=255, blank=True, null=True)
+    family_name	= models.CharField(max_length=255, blank=True, null=True)
+    pen_name = models.CharField(max_length=255, blank=True, null=True)
+    gender = models.CharField(max_length=255, blank=True, null=True)
+    country = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.person_id
+
+
+class ProgramEraGraduations(models.Model):
+    record_id = models.PositiveIntegerField(primary_key=True)
+    graduate_name = models.CharField(max_length=255, blank=True, null=True)
+    graduate_id = models.ForeignKey('ProgramEraPeople', blank=True, null=True, on_delete=models.CASCADE, related_name='graduate_id')
+    institution = models.CharField(max_length=255, blank=True, null=True)
+    advisor_name = models.CharField(max_length=255, blank=True, null=True)
+    advisor_id = models.ForeignKey('ProgramEraPeople', blank=True, null=True, on_delete=models.CASCADE, related_name='advisor_id')
+    year = models.PositiveIntegerField(blank=True, null=True)
+    program = models.CharField(max_length=255, blank=True, null=True)
+    degree = models.CharField(max_length=255, blank=True, null=True)
+    thesis_title = models.TextField(blank=True, null=True)
+    genre = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.record_id
